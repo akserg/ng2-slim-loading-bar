d4faa149db60589d6a3c2578825c2d5863571d99
Sergey Akopkokhyants <sergey@bitx.co>
2016-11-23 15:02:43 +0200

    fix(): The CommonModule removed from the import.

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@
 // https://github.com/akserg/ng2-slim-loading-bar
 
 import {NgModule, ModuleWithProviders} from "@angular/core";
-import {CommonModule} from "@angular/common";
 
 import {SlimLoadingBarComponent} from './src/slim-loading-bar.component';
 import {SlimLoadingBarService} from './src/slim-loading-bar.service';
@@ -12,9 +11,8 @@ export * from './src/slim-loading-bar.component';
 export * from './src/slim-loading-bar.service';
 
 @NgModule({
-  imports: [CommonModule],
   declarations: [SlimLoadingBarComponent],
-  exports: [CommonModule, SlimLoadingBarComponent]
+  exports: [SlimLoadingBarComponent]
 })
 export class SlimLoadingBarModule {
   static forRoot(): ModuleWithProviders {
