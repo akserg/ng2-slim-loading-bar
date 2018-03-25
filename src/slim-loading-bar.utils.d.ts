67a7c02d62b20341363946210b31a2459513d567
ssljivic <ssljivic>
2018-03-25 23:50:10 +0200

    Add all required files.

---
+++ src/slim-loading-bar.utils.d.ts
@@ -0,0 +1,4 @@
+/**
+ * Check and return true if an object not undefined or null
+ */
+export declare function isPresent(obj: any): boolean;
---
+++ src/slim-loading-bar.utils.js
@@ -0,0 +1,6 @@
+/**
+ * Check and return true if an object not undefined or null
+ */
+export function isPresent(obj) {
+    return obj !== undefined && obj !== null;
+}
